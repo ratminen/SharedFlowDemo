f7579b1af96ef9c97c66fe2868317e85107ea6ad
ratminen <ghgpoklonik16@gmail.com>
2025-11-25 19:46:56 +0700

    4. Finish project

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -17,7 +17,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 import kotlinx.coroutines.flow.SharedFlow
@@ -51,8 +53,11 @@ fun MainScreen(modifier: Modifier = Modifier, sharedFlow: SharedFlow<Int>
     val messages = remember { mutableStateListOf<Int>() }
     val lifecycleOwner = LocalLifecycleOwner.current
     LaunchedEffect (key1 = Unit) {
-        sharedFlow.collect {
-            messages.add(it)
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect {
+                println("Collecting $it")
+                messages.add(it)
+            }
         }
     }
     LazyColumn(modifier = modifier) {
